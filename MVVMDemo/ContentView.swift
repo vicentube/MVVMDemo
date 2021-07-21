8b766961b537c32f8470e74b120fc37967ee474e
Vicente Úbeda <appeleando@gmail.com>
2021-07-21 16:43:41 -0400

    Initial Commit

---
+++ MVVMDemo/ContentView.swift
@@ -0,0 +1,20 @@
+// ContentView.swift
+// MVVMDemo
+//
+// Creado el 21/7/21 por Vicente Úbeda (@vicentube)
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MVVMDemo/MVVMDemoApp.swift
@@ -0,0 +1,16 @@
+// MVVMDemoApp.swift
+// MVVMDemo
+//
+// Creado el 21/7/21 por Vicente Úbeda (@vicentube)
+// Copyright © 2021 Vicente Úbeda. Todos los derechos reservados.
+
+import SwiftUI
+
+@main
+struct MVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
